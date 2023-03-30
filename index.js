3400c2dfd4eaaf87186d7b3adac6b8a88fafaa28
Prateek <teeksingh@gmail.com>
2023-03-30 16:09:22 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blockNumber) {
-
-const scuberLocation = 42;
-return Math.abs(blockNumber - scuberLocation);
+    const scuberLocation = 42;
+    return Math.abs(blockNumber - scuberLocation);
 }
 
 function distanceFromHqInFeet(distInFeet){
@@ -13,35 +12,32 @@ function distanceFromHqInFeet(distInFeet){
 
 function distanceTravelledInFeet(start, destination){
     const feetPerBlock =  264
-    let distance
+    let distance;
      if (start >= destination){
         distance = (start - destination) * feetPerBlock
          }
     else {
-        distance = (destination - start) *feetPerBlock
+        distance = (destination - start) * feetPerBlock
         }
         return distance
 }
 
 function calculatesFarePrice(start, destination){
-    let distance   
+    let distance = distanceTravelledInFeet(start, destination);
+    
     if (distance <401){
-        fare = 0
-    }
-    else{
-        (distance >=401 && distance <=2000){
-            fare = distance * .02
-        }
-    }
-    else{
+        let fare = 0
+        return fare}
+    else if
+        (distance >=400 && distance <=2000){
+            let discountedDistance = distance - 400;
+            let fare = (discountedDistance) * .02
+            return fare}
+    else if 
         (distance >= 2000 && distance <=2499){
-            fare = 25
+            let fare = 25
+            return fare}
+    else if
+        (distance > 2500){
+            return 'cannot travel that far'}
         }
-    else{
-        distance > 2500{
-            return console.log('cannot travel that far')
-        }
-    }
-    }
-    return fare
-}
